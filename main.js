a7f4388168345f3e82a907841ad17d823e98b091
Prakt3 <Prakt3@yandex.ru>
2017-09-02 12:26:47 +0400

    w

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-for (var i; i<10 ; i++) $('.pre_box').html("<div class='battle'></div>");
+for (var i = 0; i<10 ; i++) $('.pre_box').html("<div class='battle'></div>");
 
 });
 
